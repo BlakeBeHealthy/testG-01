5b030ee1fa71f98574d3ac7fb7fe6f753a524cdc
BlakeBeHealthy Trott <blaketrott2004@gmail.com>
2026-04-20 23:26:08 -0400

    walljump is working for boss

--- testgame/Scenes/hannibal.gd
+++ testgame/Scenes/hannibal.gd
@@ -1,12 +1,15 @@
 class_name Hannibal extends CharacterBody2D
 
+@onready var as2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var state_machine: Node = $stateMachine
+@onready var wallDetection: RayCast2D = $RayCast2D
 
 @export var idle_state: HannibalState
 @export var jump_state: HannibalState
-@export var jumpStrength: float = 400
+@export var jumpStrength: float = 565
 
-var wallJump
+var direction: float = 1
+var wallJump: bool = false
 
 func _ready() -> void:
 	state_machine.init(self)
@@ -19,5 +22,12 @@ func _physics_process(delta: float) -> void:
 
 		
 func _process(delta: float) -> void:
-	print(velocity.y)
 	state_machine.process_frame(delta)
+
+func flip_direction():
+	direction *= -1
+	wallDetection.target_position.x *= -1
+	if as2d.flip_h:
+		as2d.flip_h = false
+	else:
+		as2d.flip_h = true
--- testgame/Scenes/jump.gd
+++ testgame/Scenes/jump.gd
@@ -3,8 +3,10 @@ extends HannibalState
 @onready var as2d: AnimatedSprite2D = $"../../AnimatedSprite2D"
 @onready var jump_timer: Timer = $"../../JumpTimer"
 
-var moveCheck = false
-
+var moveCheck: bool = false
+var landOver: bool = false
+var GravityReduction: float = 0.2
+var forwardMomentum: float = 300
 
 func enter() -> void:
 	as2d.play("jump")
@@ -19,21 +21,45 @@ func process_input(event: InputEvent) -> States:
 	return null
 
 func process_frame(delta: float) -> States:
-	if jump_timer.is_stopped():
-		if parent.is_on_floor():
-			print("idle")
-			return parent.idle_state
+	if parent.wallDetection.is_colliding() and !parent.is_on_floor():
+		walljump()
+		
+	if landOver:
+		return parent.idle_state
 	return null
 	
 func process_physics(delta: float) -> States:
 	if moveCheck:
 		parent.velocity.y = -parent.jumpStrength
+		parent.velocity.y = -parent.jumpStrength
 		moveCheck = false
-	else:
+	elif !parent.wallDetection.is_colliding():
 		parent.velocity.y += gravity * delta
+	else:
+		parent.velocity.y += (gravity * 0.3) * delta
+	
+	parent.velocity.x = forwardMomentum * parent.direction
 	parent.move_and_slide()
 	return null
-
-
+	
 func _on_jump_timer_timeout() -> void:
-	print("ended")
+	pass
+	
+func walljump():
+	if parent.wallJump:
+		return
+		
+	parent.wallJump = true
+	as2d.play("wallhang")
+	await get_tree().create_timer(0.1).timeout
+	parent.flip_direction()
+	as2d.play("jump")
+	if !jump_timer.is_stopped():
+		jump_timer.stop()
+	jump_timer.start()
+	moveCheck = true
+	
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if as2d.animation != "fall_attack_land":
+		return
+	landOver = true
