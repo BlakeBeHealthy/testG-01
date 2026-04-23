899aaa2f2cab637b2f822bf01bb226824d9d8582
BlakeBeHealthy Trott <blaketrott2004@gmail.com>
2026-04-22 20:11:32 -0400

    the door works for the outside just not coliding yet

--- testgame/Scenes/gate.gd
+++ testgame/Scenes/gate.gd
@@ -4,10 +4,16 @@ extends Area2D
 @onready var C2: CollisionShape2D = $CollisionShape2D
 
 @export var fliph: bool = false
+@export var closed: bool = false
 
 var open: bool = true
 
 func _ready() -> void:
+	C2.disabled = true
+	
+	if closed:
+		playOpenClose()
+		
 	if !Global.DoorChange.is_connected(playOpenClose):
 		Global.DoorChange.connect(playOpenClose)
 	
@@ -19,9 +25,15 @@ func _ready() -> void:
 		C2.position.x = 2
 		
 func playOpenClose() -> void:
+	print("DOOR")
+	open = !open
 	if !open:
 		as2d.play("Close")
 		C2.disabled = false
+		print(C2.disabled)
 	else:
 		as2d.play("Open")
 		C2.disabled = true
+
+func gateSignal():
+	Global.DoorChange.emit()
