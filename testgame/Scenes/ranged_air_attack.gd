d7f008c3b3c6cc03c03331c1e296f956278aeae9
BlakeBeHealthy Trott <blaketrott2004@gmail.com>
2026-04-24 11:56:19 -0400

    added two more projectiles after the player jumps over during chase

--- testgame/Scenes/ranged_air_attack.gd
+++ testgame/Scenes/ranged_air_attack.gd
@@ -20,24 +20,31 @@ func process_frame(delta: float) -> States:
 			var slash1 = slash_projectile.instantiate()
 			var slash2 = slash_projectile.instantiate()
 			var slash3 = slash_projectile.instantiate()
-			
+			var slash4 = slash_projectile.instantiate()
+			var slash5 = slash_projectile.instantiate()
 			slash1.rotate = 90
 			slash2.rotate = -45
 			slash3.rotate = 45
 			slash1.direction = 0
 			slash2.direction = 1
 			slash3.direction = -1
+			slash4.direction = 1
+			slash5.direction = -1
 			slash1.global_position = parent.global_position + Vector2(3 * slash1.direction, -3)
 			slash2.global_position = parent.global_position + Vector2(3 * slash2.direction, -3)
-			slash3.global_position = parent.global_position + Vector2(3 * slash2.direction, -3)
+			slash3.global_position = parent.global_position + Vector2(3 * slash3.direction, -3)
+			slash4.global_position = parent.global_position + Vector2(3 * slash4.direction, 3)
+			slash5.global_position = parent.global_position + Vector2(3 * slash5.direction, 3)
 			slash1.yChange = true
 			slash2.yChange = true
 			slash3.yChange = true
 			add_child(slash1)
 			add_child(slash2)
 			add_child(slash3)
+			add_child(slash4)
+			add_child(slash5)
 			parent.playerAbove = false
-	else:
+	elif as2d.animation_finished and !readyProjectiles:
 		parent.idle_time = 1.0
 		return parent.idle_state
 	return null
