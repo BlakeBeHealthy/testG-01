52e3466169c20b042cca0d41336fe03cabb210aa
BlakeBeHealthy Trott <blaketrott2004@gmail.com>
2026-04-23 21:03:17 -0400

    ranged class not the issue

--- testgame/Scenes/ranged_air_attack.gd
+++ testgame/Scenes/ranged_air_attack.gd
@@ -1,10 +1,12 @@
 extends HannibalState
 
-
+@onready var as2d: AnimatedSprite2D = $"../../AnimatedSprite2D"
 @onready var slash_projectile = preload("uid://lbvmh8hgo4xg")
 
+var readyProjectiles: bool = false
+
 func enter() -> void:
-	pass
+	as2d.play("fall_attack_land")
 	
 func exit() -> void:
 	pass
@@ -13,6 +15,14 @@ func process_input(event: InputEvent) -> States:
 	return null
 
 func process_frame(delta: float) -> States:
+	if readyProjectiles:
+		if parent.playerAbove:
+			var slash1 = slash_projectile.instantiate()
+			var slash2 = slash_projectile.instantiate()
+			var slash3 = slash_projectile.instantiate()
+			
+			
+		
 	return null
 
 func process_physics(delta: float) -> States:
@@ -20,4 +30,8 @@ func process_physics(delta: float) -> States:
 
 
 func _on_animated_sprite_2d_frame_changed() -> void:
-	pass # Replace with function body.
+	if as2d.animation != "fall_attack_land" and parent.state_machine.current_state != parent.airAttack_State:
+		return
+		
+	if as2d.frame == 1:
+		readyProjectiles = true
